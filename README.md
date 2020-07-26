7ec61ab100f55715ded601a00641a352cea38a0a
pycnonotus <Anton.golo.fh@gmail.com>
2020-07-27 00:21:44 +0300

    made the script work :)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 A sample web scraper using nodejs and puppeteer
+
+
+##TODO:
+[ ] make it async 
--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const urls = require('./pokemonsUrl.json');
 async function run() {
 
     const browser = await puppeteer.launch({
-        headless: true // set true if don't want to see the browser
+        headless: false // set true if don't want to see the browser
     });
 
     const page = await browser.newPage();
@@ -30,23 +30,41 @@ async function run() {
             waitUntil: 'load'
         });
 
-        let data = await page.evaluate(() => {
+        let data = await page.evaluate((i) => {
             let thumbnail = document.querySelector('.single-post-thumb img').src; // big image
             let img = document.querySelector('#main-content .entry p img').src; // small image
-            // let selector = document.querySelectorAll('#main-content .entry p img tbody');
-            //  let name = selector[0].querySelectorAll("tr")[1].querySelectorAll("td")[0].innerHTML;
-
-
+            let selector = document.querySelectorAll('#main-content .entry  tbody');
+            let name = selector[0].querySelectorAll("tr")[1].querySelectorAll("td")[1].innerHTML;
+            let hebrewName = selector[0].querySelectorAll("tr")[1].querySelectorAll("td")[1].innerHTML;
+            let type = selector[2].querySelectorAll("tr")[1].querySelectorAll("td")[1].innerHTML;
+            let ev = selector[3].querySelectorAll("tr")[1].querySelectorAll("td")[0].innerHTML;
+            let catchRate = selector[3].querySelectorAll("tr")[1].querySelectorAll("td")[1].innerHTML;
+            let happiness = selector[3].querySelectorAll("tr")[1].querySelectorAll("td")[2].innerHTML;
+            let baseStats = {
+                hp: selector[5].querySelectorAll("tr")[1].querySelectorAll("td")[0].innerHTML,
+                atk: selector[5].querySelectorAll("tr")[1].querySelectorAll("td")[1].innerHTML,
+                def: selector[5].querySelectorAll("tr")[1].querySelectorAll("td")[2].innerHTML,
+                s_atk: selector[5].querySelectorAll("tr")[1].querySelectorAll("td")[3].innerHTML,
+                s_def: selector[5].querySelectorAll("tr")[1].querySelectorAll("td")[4].innerHTML,
+                spd: selector[5].querySelectorAll("tr")[1].querySelectorAll("td")[5].innerHTML,
+                total: selector[5].querySelectorAll("tr")[1].querySelectorAll("td")[6].innerHTML,
+            };
             return {
                 "thumbnail": thumbnail,
                 image: img,
-                //"id": id,
-                //"name": name
+                id: i,
+                "name": name,
+                hebrewName: hebrewName,
+                type: type,
+                ev: ev,
+                catchRate: catchRate,
+                happiness: happiness,
+                baseStats: baseStats
             };
-        });
+        }, i);
 
-
-
+        i++;
+        console.log(data);
         pokemons.info.push(data);
     }
     console.log("done ");
